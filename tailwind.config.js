42b3db62a0b913d9236daad9a3e3863c8af588fb
ande kishore kumar <ande.kishore007@gmail.com>
2024-06-21 10:38:00 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px,',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12,88%,59%)',
+        polymerAsh: '#f4f4f4',
+        polymerWhiteButtonBg: '#E7E7E7',
+        white: '#ffffff',
+        darkGrayishBlue: 'hsl(227,12%, 61%)',
+        polymerPurple: '#FBD7FF',
+        polymerYellow: '#FFDEC1',
+      },
+      backgroundImage: (theme) => ({
+        customGradient:
+          'linear-gradient(to bottom right, #fbd7ff 10%, #ffdec1 80%)',
+      }),
+    },
+  },
+  plugins: [],
+};
